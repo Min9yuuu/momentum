afdb73ddca7e5d79cbafbabe32a93b4de560f2fd
Min9yu <97788585+Min9yuuu@users.noreply.github.com>
2022-05-09 16:17:20 +0900

    Added Login

---
+++ js/login.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onSubmit);
+} else {
+  paintGreetings(savedUserName);
+}

347f26a63c1f7e1ea72afa194db436f2ee90a94a
Min9yu <97788585+Min9yuuu@users.noreply.github.com>
2022-05-10 18:09:01 +0900

    Added stlye

--- js/background.js
+++ js/background.js
@@ -1,8 +1,7 @@
 const images = ["0.jpeg", "1.png", "2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
+const bgImage = `url(img/${chosenImage})`;
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.prepend(bgImage);
+document.body.style.backgroundImage = bgImage;
+document.body.style.backgroundSize = "100vw 100vh";
